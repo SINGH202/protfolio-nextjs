da03c7906ea8dfe0b510433aa87b8f5cb744a68b
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-06-30 15:30:21 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+        First change
+      </main>
   );
 }
